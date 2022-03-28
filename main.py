6fb0e152e32baded503d06446926bcc49f9587a0
uiop123414 <klkejo@gmail.com>
2022-03-28 21:48:55 +0300

    Some changes in code

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import finviz
 import pprint
+import sys
 
 
 def main():
-    a = finviz.get_stock('MSFT')
+    a = None
+    try:
+        a = finviz.get_stock('jklsfd')
+    except Exception:
+        print(f"invalid name")
+        sys.exit()
+
     print(finviz.Portfolio)
     pprint.pprint(a.get('P/C'))
-
+    pprint.pprint(a.keys())
 
 
 if __name__ == '__main__':
